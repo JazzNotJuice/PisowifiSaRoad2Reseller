20b6dc3b9fb538544831846da86c35dc55c56a3a
JazzNotJuice <juzealdepaz@gmail.com>
2025-01-07 22:23:59 +0800

    Update print.php

--- voucher/print.php
+++ voucher/print.php
@@ -203,6 +203,24 @@ if ($userp != "") {
 ?>
 <?php 
 } ?>
+
+ <script>
+function sendToQuickPrinter(){
+    var text = "test printer<br><big>Big title<br><cut>";
+    var textEncoded = encodeURI(text);
+    window.location.href="quickprinter://"+textEncoded;
+}
+
+//if you are using latest version of chrome browser I recommend to use:
+function sendToQuickPrinterChrome(){
+    var text = "test printer<br><big>Big title<br><cut>";
+    var textEncoded = encodeURI(text);
+    window.location.href="intent://"+textEncoded+"#Intent;scheme=quickprinter;package=pe.diegoveloper.printerserverapp;end;";
+    
+}
+</script>
+
+<a onclick="sendToQuickPrinter();">Print Button</a>
 	
 </body>
 </html>
