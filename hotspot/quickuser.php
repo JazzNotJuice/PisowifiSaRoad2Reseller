3a35b55d8be1c04cff80d3c9e0beaba6f7aae48f
JazzNotJuice <juzealdepaz@gmail.com>
2025-01-07 22:46:05 +0800

    Update quickuser.php

--- hotspot/quickuser.php
+++ hotspot/quickuser.php
@@ -272,7 +272,7 @@ if ($currency == in_array($currency, $cekindo['indo'])) {
 			//		sendToQuickPrinterChrome();
   			//} else if (w > 800) {
 
-					window.open('./voucher/print.php?user=<?= $usermode ?>-<?= $uname ?>&qr=<?= $qr ?>&session=<?= $session ?>','_blank','width=310,height=450').print();
+					window.open('./voucher/print.php?user=<?= $usermode ?>-<?= $uname ?>&qr=<?= $qr ?>&session=<?= $session ?>','_blank','width=310,height=450');
 					//window.location.href="./?hotspot-user=<?= $u[1] ?>&session=<?= $session ?>";
   			//}
     //sendToQuickPrinterChrome();
