e88a5c7bc35fd025edf2d65f5ce7d082ba331ca0
JazzNotJuice <juzealdepaz@gmail.com>
2025-01-07 16:31:43 +0800

    Update printbt.php

--- voucher/printbt.php
+++ voucher/printbt.php
@@ -10,25 +10,30 @@ function sendToQuickPrinterChrome(){
                         //"<line0>\n" +
                         "<center><?= date("Y-m-d h:i:sa") ?>\n" +
                         "<line0>\n" +
-                        <?php
-                        echo '"'.$username.' :;; '. $username.'\n" +';
-                        ?>
-                        <?php
-                          echo '"'.$validity.' :;; '.$validity.'\n" +';
-                        ?>
-                        <?php 
-                          echo '"'.$timelimit.' :;; '.$timelimit.'\n" +';
-                        ?>
-                        <?php 
-                          echo '"'.$datalimit.' :;; '.$datalimit.'\n" +';
-                        ?>
-                        <?php
-                          echo '"'.$price.' :;; '.$price.'\n" +';
-                        ?>
+                        <?php if($uname == $upass){
+                        echo '"'.$_voucher_code.' :;; '. $uname.'\n" +';
+                        }else{
+                          echo '"'.$_user_name.' :;; '. $uname.'\n" +
+                        "'.$_password.' :;; '. $upass.'\n" +';
+                        }?>
+                        <?php if($getvalid != ""){
+                          echo '"'.$_validity.' :;; '.$validity.'\n" +';
+                        }?>
+                        <?php if($utimelimit != ""){
+                          echo '"'.$_time_limit.' :;; '.$timelimit.'\n" +';
+                        }?>
+                        <?php if($udatalimit != ""){
+                          echo '"'.$_data_limit.' :;; '.$udatalimit.$MG.'\n" +';
+                        }?>
+                        <?php if ($getprice != 0) {
+                          echo '"'.$_price.' :;; '.$pricebt.'\n" +';
+                        }?>
                         "<line0>\n" +
-                        "<center>Login : \n" +
+                        "<center>Login : http://<?= $dnsname ?>\n" +
                         //"<line0>\n" +
-                        <?php echo '"\n"';?>
+                        <?php if($qrbt == "enable"){
+                          echo '"<center><image>" + "'.$qrcode.'" +"\n"';
+                        }else{echo '"\n"';}?>
                         + "<cut>"
                 ;
     var textEncoded = encodeURI(commandsToPrint);
