e1e33b2a75c0fe39fedb0b495b096a2e6e551396
JazzNotJuice <juzealdepaz@gmail.com>
2025-01-07 23:35:51 +0800

    Update default-thermal.php

--- voucher/default-thermal.php
+++ voucher/default-thermal.php
@@ -1,19 +1,44 @@
-
+																								
 <style>
-	.qrcode{
-		height:100px;
-		width:100px;
-	}
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+  }
+
+  .voucher {
+    width: 250px; /* Increase width */
+    padding: 10px;
+  }
+
+  .voucher td {
+    text-align: center;
+    font-size: 18px; /* Increase font size */
+  }
+
+  .voucher .qrcode {
+    height: 180px; /* Increase QR code size */
+    width: 180px;
+  }
 </style>
 
-<table class="voucher" style=" width: 180px;">
+<table class="voucher" style=" width: 100%;">
   <tbody>
 <!-- Logo Hotspotname -->
     <tr>
-      <td style="text-align: center; font-size: 14px; font-weight:bold;"><?= $hotspotname; ?></td>
+      <td style="text-align: center; font-size: 22px; font-weight:bold;"><?= $hotspotname; ?> (Store 1)</td>
     </tr>
     <tr>
-      <td style="text-align: center; font-size: 14px; font-weight:bold; border-bottom: 1px black solid;"><img src="<?= $logo; ?>" alt="logo" style="height:30px;border:0;"><br><?= date("Y-m-d h:i:sa") ?></td>
+      <td style="text-align: center; font-size: 18px; font-weight:bold;"><?= date("Y-m-d h:i:sa") ?></td>
+    </tr>
+	<tr>
+      <td style="text-align: center; font-size: 30px; font-weight:bold; border-bottom: 1px black solid; border-top: 1px black solid;"><br><?= $username; ?><br><br></td>
+    </tr>
+	<tr>
+      <td style="text-align: center; font-size: 22px; font-weight:bold;"><?= $validity; ?> <?= $price; ?></td>
     </tr>
 <!-- /  -->
     <tr>
@@ -23,15 +48,10 @@
 <!-- Username Password QR    -->
     <tr>
       <td>
-        <table style="width:100%;">
+        <table style="width:100%; ">
 <!-- Username = Password    -->
 <?php if ($usermode == "vc") { ?>
-        <tr>
-          <td font-size: 12px;>Kode Voucher</td>
-        </tr>
-        <tr>
-          <td style="width:100%; border: 1px solid black; font-weight:bold; font-size:16px;"><?= $username; ?></td>
-        </tr>
+       
 <!-- /  -->
 <!-- Username & Password  -->
 <?php 
@@ -58,20 +78,12 @@
 <?php 
 } ?>
 <!-- /  -->
-    <tr>
-      <!-- Price  -->
-      <td colspan="2" style="border-top: 1px solid black;font-weight:bold; font-size:16px"><?= $validity; ?> <?= $timelimit; ?> <?= $datalimit; ?> <?= $price; ?></td>
-<!-- /  -->
-    </tr>
-    <tr>
-      <!-- Note  -->
-      <td colspan="2" style="font-weight:bold; font-size:12px">Login: http://<?= $dnsname; ?></td>
-<!-- /  -->
-    </tr>
+   
+   
 <!-- /  -->
   </tbody>
     </table>
       </td>
     </tr>
   </tbody>
-</table>
+</table>	        	        	        	        
